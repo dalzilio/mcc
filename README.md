1c23533f2ef041f12ff185887f756faeab3b8d37
Silvano Dal Zilio <dalzilio@laas.fr>
2017-11-23 17:15:25 +0100

    adding the lola command

--- README.md
+++ README.md
@@ -1,34 +1,52 @@
 # MCC
 
-High-Level Nets Blaster for the Model-Checking Contest.
+MCC is a High-Level Net "Blaster" for the Model-Checking Contest (MCC).
+
+We use the term *blaster*, instead of *unfolding*, to underline the fact that
+our translation is very crude. A comparable tool is *andl*, distributed with
+[Marcie]("http://www-dssz.informatik.tu-cottbus.de/DSSZ/Software/Marcie"). In
+the future, we plan to use the transformation to compute interesting properties
+of the models, like symetries and/or set of places that can be clustered
+together.
+
+At the moment, we simply recognize the case of high-level nets with a single
+variable used with a circular symmetry (basically a "scalar set"). This allows us to manage very big instances of the Philosophers model.
 
 ## Getting Started
 
-Before building the tool you need to install a Go distribution at <https://golang.org/doc/install>.
-
-### Installing
-
-You can simply install the software using the *go vet* command then *go install*.
-
-We use [Cobra]("https://github.com/spf13/cobra") to generate the CLI, hence the strange syntax.
+Before building the tool you need to install a recent  Go distribution at
+<https://golang.org/doc/install>. Then you can install the software using the
+*go get* command.
 
 ```bash
 go get github.com/dalzilio/mcc
-go install github.com/dalzilio/mcc/cpn/cmd/mcc
 ```
 
 ## Running the program
 
-Download a *model.pnml* file (high-level net, also tagged COL) from the MCC repository. The *hlnet* command can be invoked as follows.
+The *mcc hlnet* command accepts PNML files for high-level nets provided by the MCC (also tagged as COL).
+These files generally have the name *model.pnml*.
+You can invoke the *hlnet* command on this file as follows.
 
 ```bash
-mcc hlnet -i model.pnml
+$> mcc hlnet -i model.pnml
 ```
 
-To find help on a command simply type
+You can obtain info on the other available options by using the *help* command:
 
-```bash
-mcc help hlnet
+```text
+$> mcc help
+collection of tools for the MCC
+
+Usage:
+  mcc [command]
+
+Available Commands:
+  hlnet       generates a .net or .tpn file from a PNML file describing a high-level net
+  lola        generates a net file in the LoLa format from a PNML file describing a high-level net
+
+Use "mcc [command] --help" for more information about a command.
+$> mcc help hlnet
 ```
 
 ## License
---
+++ cmd/lola.go
@@ -0,0 +1,102 @@
+// Copyright 2017. LAAS-CNRS, Vertics. All rights reserved.
+// Use of this source code is governed by the CeCILL-B license
+// that can be found in the LICENSE file.
+
+package cmd
+
+import (
+	"github.com/dalzilio/mcc/corenet"
+	"github.com/dalzilio/mcc/hlnet"
+	"github.com/dalzilio/mcc/pnml"
+
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/cobra"
+)
+
+// lolaCmd takes a a PNML model for a high level net, with a .pnml extension,
+// and generates a .net or .tpn file with the same basename. The command returns
+// an error for  PNML files that contain a core net description.
+var lolaCmd = &cobra.Command{
+	Use:   "lola -i file.pnml",
+	Short: "generates a net file in the LoLa format from a PNML file describing a high-level net",
+	Run: func(cmd *cobra.Command, args []string) {
+		lolaConvert(lolaFileName)
+	},
+}
+
+var lolaFileName string
+var lolaOutFileName string
+var lolaUseName bool
+var lolaLogger *log.Logger
+
+func init() {
+	RootCmd.AddCommand(lolaCmd)
+	lolaCmd.Flags().StringVarP(&lolaFileName, "file", "i", "", "name of the input file (.pnml)")
+	lolaCmd.Flags().StringVarP(&lolaOutFileName, "out", "o", "", "basename of the output file (without extension, default to input file basename)")
+	lolaCmd.Flags().BoolVar(&lolaUseName, "name", false, "use PNML (document) name for the output file")
+
+	lolaLogger = log.New(os.Stderr, "MCC LOLA:", 0)
+}
+
+func lolaConvert(filename string) {
+	// we capture panics so that that we don't pollute stdout if something goes wrong
+	defer func() {
+		if r := recover(); r != nil {
+			lolaLogger.Println("Error in generation: cannot compute")
+			os.Exit(1)
+		}
+	}()
+
+	if filename == "" {
+		lolaLogger.Println("Bad command line! Input file mandatory. Use option -i")
+		os.Exit(1)
+		return
+	}
+
+	if filepath.Ext(filename) != ".pnml" {
+		lolaLogger.Println("Wrong file extension!")
+		os.Exit(1)
+		return
+	}
+
+	xmlFile, err := os.Open(filename)
+	if err != nil {
+		lolaLogger.Println("Error opening file:", err)
+		os.Exit(1)
+		return
+	}
+	defer xmlFile.Close()
+
+	decoder := pnml.NewDecoder(xmlFile)
+	var p = new(pnml.Net)
+	err = decoder.Build(p)
+	if err != nil {
+		lolaLogger.Println("Error decoding PNML file:", err)
+		os.Exit(1)
+		return
+	}
+
+	// compute name of the output file. There are three possible cases
+	_, file := filepath.Split(filename)
+
+	outfile := lolaOutFileName
+	if outfile == "" {
+		outfile = file[0 : len(file)-len(".pnml")]
+	}
+	if lolaUseName {
+		outfile = p.Name
+	}
+
+	p.SetVerbose(pnml.MINIMAL)
+	// set the semantic of "overflowing" enumeration types
+	p.SetFES(false)
+	hl := hlnet.Build(p)
+
+	cn := corenet.Build(p, hl)
+	outfile = outfile + ".net"
+	ioutil.WriteFile(outfile, []byte(cn.Lola()), 0755)
+}
