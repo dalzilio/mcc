0e36c2f152e9074bff68b0bbbe0f622b896e5690
Silvano Dal Zilio <xxx>
2018-03-20 16:54:39 +0100

    added new examples from the MCC in the benchmarks and optimized the printing routine (writing to the net file)

---
+++ corenet/write.go
@@ -0,0 +1,52 @@
+// Copyright 2017. LAAS-CNRS, Vertics. All rights reserved.
+// Use of this source code is governed by the CeCILL-B license
+// that can be found in the LICENSE file.
+
+package corenet
+
+import (
+	"fmt"
+	"io"
+)
+
+// ----------------------------------------------------------------------
+
+func (pl Place) Write(w io.Writer) {
+	if pl.init == 0 {
+		fmt.Fprintf(w, "pl p%d\n", pl.count)
+		return
+	}
+	fmt.Fprintf(w, "pl p%d (%d)\n", pl.count, pl.init)
+}
+
+func (pl corep) Write(w io.Writer) {
+	if pl.int == 1 {
+		fmt.Fprintf(w, " p%d", pl.count)
+		return
+	}
+	fmt.Fprintf(w, " p%d*%d", pl.count, pl.int)
+}
+
+func (tr Trans) Write(w io.Writer) {
+	fmt.Fprintf(w, "tr t%d ", tr.count)
+	for _, v := range tr.in {
+		v.Write(w)
+	}
+	fmt.Fprint(w, " ->")
+	for _, v := range tr.out {
+		v.Write(w)
+	}
+	fmt.Fprint(w, "\n")
+}
+
+// Write outputs the corenet in .net format on an io.Writer.
+func (net Net) Write(w io.Writer) {
+	fmt.Fprintf(w, "#net %s has %d places and %d transitions\n", net.name, len(net.pl), len(net.tr))
+	fmt.Fprintf(w, "net {%s}\n", net.name)
+	for _, v := range net.pl {
+		v.Write(w)
+	}
+	for _, v := range net.tr {
+		v.Write(w)
+	}
+}
